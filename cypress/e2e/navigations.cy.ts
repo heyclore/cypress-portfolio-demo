83641c7f3249e823840a86839141c4007eb3ec2f
heyclore <cloore@gmail.com>
2026-01-01 08:50:49 +0700

    add navigation tests

--- cypress/e2e/navigations.cy.ts
+++ cypress/e2e/navigations.cy.ts
@@ -1,6 +1,43 @@
-describe("My First Test", () => {
-  it("Visits example.com", () => {
+import { NavbarButton } from "../locators/navbarComponents";
+
+describe("Navbar navigation", () => {
+  beforeEach(() => {
     cy.visit("https://docs.cypress.io/");
-    cy.title().should("include", "testing");
+  });
+
+  it("navigate to Api page", () => {
+    cy.get(NavbarButton.ApiButton).click();
+    cy.title().should("include", "API");
+    cy.get(NavbarButton.ApiButton).should("have.attr", "aria-current", "page");
+  });
+
+  it("navigate to Cloud page", () => {
+    cy.get(NavbarButton.CloudButton).click();
+    cy.title().should("include", "Cloud");
+    cy.get(NavbarButton.CloudButton).should(
+      "have.attr",
+      "aria-current",
+      "page",
+    );
+  });
+
+  it("navigate to Ui Coverage page", () => {
+    cy.get(NavbarButton.UiCoverageButton).click();
+    cy.title().should("include", "UI Coverage");
+    cy.get(NavbarButton.UiCoverageButton).should(
+      "have.attr",
+      "aria-current",
+      "page",
+    );
+  });
+
+  it("navigate to Accessibility page", () => {
+    cy.get(NavbarButton.AccessibilityButton).click();
+    cy.title().should("include", "Accessibility");
+    cy.get(NavbarButton.AccessibilityButton).should(
+      "have.attr",
+      "aria-current",
+      "page",
+    );
   });
 });
---
+++ cypress/locators/navbarComponents.ts
@@ -0,0 +1,7 @@
+export enum NavbarButton {
+  AppButton = '.navbar__items a[href="/app/get-started/why-cypress"]',
+  ApiButton = '.navbar__items a[href="/api/table-of-contents"]',
+  CloudButton = '.navbar__items a[href="/cloud/get-started/introduction"]',
+  UiCoverageButton = '.navbar__items a[href="/ui-coverage/get-started/introduction"]',
+  AccessibilityButton = '.navbar__items a[href="/accessibility/get-started/introduction"]',
+}
